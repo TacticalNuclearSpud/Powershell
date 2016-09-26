1ea4d0f2f5f0f094eb31bdd91c9c4b5adf7e56a2
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-09-26 13:01:21 +0100

    Adds comment based help and renames script

---
+++ Clean-RDServer.ps1
@@ -0,0 +1,35 @@
+﻿<#
+.SYNOPSIS
+
+Clean-RSServer removes leftover profiles from Remote Desktop Services Farm Server. 
+.DESCRIPTION
+Clean-RDServer pulls a list of leftover roaming profiles from the named server and removes them, with the exception of my own admin account
+.PARAMETER computername
+The name of the Remote Desktop Services Server to clean.
+.EXAMPLE
+Clean-RDServer -ComputerName WMT-RDS01
+#>
+[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory=$True)]
+    [string]$computername
+)
+#list user profiles excluding service accounts any local accounts and my own admin account.
+Get-WmiObject -Class win32_userprofile -ComputerName $computername | `
+    Where-Object -Filter {$_.RoamingConfigured -eq $true} | `
+        Where-Object -Filter {$_.LocalPath -notlike '*mike.brankin.admin*'} | `
+
+        #loop through profiles, remove profile and delete folder.
+        ForEach-Object {
+            
+            $profileSID = New-Object System.Security.Principal.SecurityIdentifier($_.SID)
+            $currentProfile = ($profileSID.Translate([System.Security.Principal.NTAccount])).Value
+            write-host $currentProfile
+            
+            #delete profile
+            #$_.Delete()
+
+        }
+        
+      
