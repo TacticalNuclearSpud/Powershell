92333e477c516a0d3d62083cd59c930ce393e68e
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-09-28 11:29:57 +0100

    changes folder structure to organise scripts

--- paralleltest_workflow.ps1
+++
@@ -1,8 +0,0 @@
-﻿workflow paralleltest {
-    parallel {
-        Get-CimInstance -ClassName Win32_OperatingSystem
-        get-process -Name powershell*
-        Get-CimInstance -ClassName Win32_OperatingSyatem
-        Get-Service -Name s*
-        }
-}
