1177984c9d29a89e9d39b40f158da547447855ea
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-09-29 08:21:12 +0100

    adds bare bones of script to automatically build lab vm's

---
+++ Hyper-V/Build-Lab.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.SYNOPSIS
+
+.DESCRIPTION
+
+.PARAMETER vmname
+
+.PARAMETER master
+
+.PARAMETER vhdpath
+
+.EXAMPLE
+
+#>
+[CmdletBinding()]
+
+param (
+    [Parameter(Mandatory=$True)]
+    [array]$vmname,
+
+    [Parameter(Mandatory=$True)]
+    [string]$master,
+
+    [Parameter(Mandatory=$True)]
+    [array]$vhdpath
+   
+
+)
+$vmname | Foreach-object {
+
+    New-VM -Name $_ -MemoryStartupBytes 800MB -Generation 2 -BootDevice VHD -SwitchName Contoso -VHDPath "F:\VHDS\$_.vhdx"
+    Set-VMMemory -VMName $_ -DynamicMemoryEnabled $True -MinimumBytes 512MB -MaximumBytes 800MB
+
+    }
