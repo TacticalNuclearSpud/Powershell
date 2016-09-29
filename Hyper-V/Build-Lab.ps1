9aa09b143a295df583d53aeda18597ae179ccf7e
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-09-29 09:13:59 +0100

    adds verbose output
    added lines to write verbose output while script runs

--- Hyper-V/Build-Lab.ps1
+++ Hyper-V/Build-Lab.ps1
@@ -4,13 +4,16 @@
 .DESCRIPTION
 
 .PARAMETER vmname
+Name of VM or VMs you wish to create.
 
 .PARAMETER master
+Path to master vhd
 
 .PARAMETER vhdpath
+Path to VHD storage
 
 .EXAMPLE
-
+Build-lab -VMName SERVER-1, SERVER-2 -Master "E:\MASTER.vhdx" -VHDPath "E:\VHDS\"
 #>
 [CmdletBinding()]
 
@@ -26,9 +29,23 @@ param (
    
 
 )
+
 $vmname | Foreach-object {
 
-    New-VM -Name $_ -MemoryStartupBytes 800MB -Generation 2 -BootDevice VHD -SwitchName Contoso -VHDPath "F:\VHDS\$_.vhdx"
+    #set destination directory for new vhdx files
+    $destination = "$vhdpath$_.vhdx"
+
+    #copy and rename master image to vhd path
+    Write-Verbose "Copying $master to $destination"
+    Copy-Item -Path $master -Destination $destination
+
+    #create VM's
+    Write-Verbose "Creating $_ VM"
+    New-VM -Name $_ -MemoryStartupBytes 800MB -Generation 2 -BootDevice VHD -SwitchName Contoso -VHDPath $destination
+
+    #Set dynamic memory, min and max
+    Write-Verbose "Setting dynamic memory min and max for VM $_"
     Set-VMMemory -VMName $_ -DynamicMemoryEnabled $True -MinimumBytes 512MB -MaximumBytes 800MB
+    
 
     }
