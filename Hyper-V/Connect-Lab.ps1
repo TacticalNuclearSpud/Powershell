741cae7e8ec02d93a4104b37e1356bf024c206e7
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-09-30 06:46:39 +0100

    adds script for connecting to lab-host
    enters pssession to lab-host and creates new drive that connects to
    script library on laptop.

---
+++ Hyper-V/Connect-Lab.ps1
@@ -0,0 +1,5 @@
+﻿$credential = Get-Credential
+$computername = "192.168.0.100"
+Enter-PSSession -ComputerName $computername -Credential $credential
+
+New-PSDrive -Name S -PSProvider FileSystem -Root "\\wmt-lwd-lpt33\Powershell" -Credential "wmt-lwd-lpt33\mike" -Persist
