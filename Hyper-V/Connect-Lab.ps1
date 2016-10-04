0b541916faf6d5538fd3a68a7d5f299816ffbc4c
TacticalNuclearSpud <Mike_brankin@hotmail.com>
2016-10-04 07:45:26 +0100

    Removes old lines

--- Hyper-V/Connect-Lab.ps1
+++ Hyper-V/Connect-Lab.ps1
@@ -11,14 +11,13 @@ function Connect-Lab
 {
     $credential = Get-Credential
 
+    #Lab-host-01 is workgroup server, needs IP to connect.
     $labaddress = "192.168.0.100"
 
     #name of technician machine
     $computername = "wmt-lwd-lpt33"
 
-    #Lab-host-01 is workgroup server, needs IP to connect.
-    #Enter-PSSession -ComputerName $labaddress #-Credential $credential
-    $session = New-PSSession -ComputerName $labaddress #-Credential $credential
+    $session = New-PSSession -ComputerName $labaddress -Credential $credential
  
     Invoke-Command -Session $session -ScriptBlock { param($computername) New-PSDrive -PSProvider FileSystem -Name "S" -Root "\\$computername\Powershell" -Persist -Credential "$computername\mike"} -ArgumentList $computername
 
